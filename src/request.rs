3dce4a0dd28ce9cd3b0378ff2b357272fd2f9cd4
Xie Yuanhang <1021274364@qq.com>
2022-03-06 22:35:08 +0800

    feat: detect web charset and decode

--- src/request.rs
+++ src/request.rs
@@ -1,19 +1,59 @@
+use chardetng::EncodingDetector;
+use egui::TextBuffer;
 use http::StatusCode;
 use openssl::ssl::{SslConnector, SslMethod};
 use std::{
+    borrow::Cow,
     collections::HashMap,
     error::Error,
     fs,
     io::{BufRead, BufReader, Read, Write},
     net::TcpStream,
+    str,
     str::FromStr,
 };
 
+const detect_step: usize = 100;
 #[derive(Default)]
 pub struct Response {
     pub status: StatusCode,
     pub headers: Option<HashMap<String, String>>,
     pub body: Option<Vec<u8>>,
+}
+
+impl Response {
+    pub fn body_to_string(&self) -> Result<Cow<str>, Box<dyn Error>> {
+        match &self.body {
+            Some(body) => match str::from_utf8(body) {
+                Ok(body) => Ok(Cow::Borrowed(body)),
+                Err(_) => {
+                    let mut guess_detector = EncodingDetector::new();
+                    let mut body_iter = body.chunks(self::detect_step).peekable();
+                    let mut decode_body: Option<Cow<str>> = None;
+                    while let Some(part) = body_iter.next() {
+                        let finished = body_iter.peek().is_none();
+                        let meet_non_ascll = guess_detector.feed(part, finished);
+                        if !meet_non_ascll {
+                            continue;
+                        }
+                        let guess_charset = guess_detector.guess(None, false);
+                        let (body_str, _, has_error) = guess_charset.decode(body);
+                        if !has_error {
+                            decode_body = Some(body_str);
+                            break;
+                        }
+                    }
+
+                    if let Some(body_str) = decode_body {
+                        Ok(body_str)
+                    } else {
+                        Err("Cannot Guess Charset")?
+                    }
+                }
+            },
+            None => Ok(Cow::Borrowed("")),
+        }
+    }
 }
 
 fn request(url: &str) -> Result<Response, Box<dyn Error>> {
@@ -26,6 +66,7 @@ fn request(url: &str) -> Result<Response, Box<dyn Error>> {
             status: StatusCode::OK,
             headers: None,
             body: Some(fs::read(url)?),
+            ..Default::default()
         });
     }
     if schema != "https" && schema != "http" {
@@ -75,6 +116,7 @@ fn online_access<S: Read + Write>(
             status: StatusCode::from_str(status)?,
             headers: None,
             body: Some(Vec::from(explanation)),
+            ..Default::default()
         });
     }
     let mut headers: HashMap<String, String> = HashMap::new();
@@ -92,23 +134,19 @@ fn online_access<S: Read + Write>(
         status: StatusCode::from_str(status)?,
         headers: Some(headers),
         body: Some(body),
+        ..Default::default()
     });
 }
 
 fn show(resp: Response) -> Result<String, Box<dyn Error>> {
     if resp.status == StatusCode::OK {
-        if let Some(body) = resp.body {
-            show_only_body(body)
-        } else {
-            Ok(String::new())
-        }
+        show_only_body(resp.body_to_string()?)
     } else {
-        show_without_tag(resp.status, resp.body.unwrap())
+        show_without_tag(resp.status, resp.body_to_string()?)
     }
 }
 
-fn show_without_tag(status: StatusCode, body: Vec<u8>) -> Result<String, Box<dyn Error>> {
-    let body = String::from_utf8(body)?;
+fn show_without_tag(status: StatusCode, body: Cow<str>) -> Result<String, Box<dyn Error>> {
     let mut is_angle = 0;
     let mut body_without_tag = status.to_string();
     for c in body.chars() {
@@ -131,13 +169,13 @@ fn parse_data(url: &str) -> Result<Response, Box<dyn Error>> {
             status: StatusCode::OK,
             headers: None,
             body: Some(Vec::from(format!("<html><body>{}</body></html>", body))),
+            ..Default::default()
         }),
         _ => Err("Not Support Data Meta Type")?,
     }
 }
 
-fn show_only_body(body: Vec<u8>) -> Result<String, Box<dyn Error>> {
-    let body = String::from_utf8(body)?;
+fn show_only_body(body: Cow<str>) -> Result<String, Box<dyn Error>> {
     let mut is_angle = 0;
     let mut meet_body = false;
     let mut leave_body = false;
@@ -158,7 +196,7 @@ fn show_only_body(body: Vec<u8>) -> Result<String, Box<dyn Error>> {
         } else if is_angle == 0 && meet_body && !leave_body {
             only_body.push(c);
         } else if is_angle == 1 {
-            tag.push(char::from(c));
+            tag.push(c);
         }
     }
     Ok(only_body)
