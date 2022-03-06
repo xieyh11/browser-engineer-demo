48ed9de5e2f3714a004b2555809b704a802c269f
Xie Yuanhang <1021274364@qq.com>
2022-03-06 13:59:36 +0800

    feat: more reliable

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,18 @@ mod request;
 
 use crate::request::load;
 use eframe::{egui, epi};
-use std::env;
+use std::{env, error::Error};
 
 #[derive(Default)]
 struct Browser {
-    url: String
+    url: String,
 }
 
-fn selectable_text(ui: &mut egui::Ui, mut text: &str) {
+fn selectable_text(ui: &mut egui::Ui, text: Result<String, Box<dyn Error>>) {
+    let mut text = match text {
+        Ok(t) => t,
+        Err(e) => e.to_string(),
+    };
     ui.add(
         egui::TextEdit::multiline(&mut text)
             .desired_width(f32::INFINITY)
@@ -24,12 +28,12 @@ impl epi::App for Browser {
 
     fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.vertical(|ui|{
+            ui.vertical(|ui| {
                 ui.label(&self.url);
                 ui.add_space(10.0);
                 ui.separator();
-                egui::ScrollArea::vertical().show(ui, |ui|{
-                    selectable_text(ui, &load(&self.url));
+                egui::ScrollArea::vertical().show(ui, |ui| {
+                    selectable_text(ui, load(&self.url));
                 });
             });
         });
@@ -37,7 +41,9 @@ impl epi::App for Browser {
 }
 
 fn main() {
-    let app = Browser{url: env::args().nth(1).unwrap()};
+    let app = Browser {
+        url: env::args().nth(1).unwrap(),
+    };
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(Box::new(app), native_options);
 }
--- src/request.rs
+++ src/request.rs
@@ -2,25 +2,34 @@ use http::StatusCode;
 use openssl::ssl::{SslConnector, SslMethod};
 use std::{
     collections::HashMap,
+    error::Error,
     fs,
-    io::{BufRead, BufReader, Error, Read, Write},
+    io::{BufRead, BufReader, Read, Write},
     net::TcpStream,
+    str::FromStr,
 };
 
-fn request(url: &str) -> Result<Option<(String, HashMap<String, String>, String)>, Error> {
+#[derive(Default)]
+pub struct Response {
+    pub status: StatusCode,
+    pub headers: Option<HashMap<String, String>>,
+    pub body: Option<Vec<u8>>,
+}
+
+fn request(url: &str) -> Result<Response, Box<dyn Error>> {
     if url.starts_with("data:") {
         return parse_data(url);
     }
     let (schema, url) = url.split_once("://").unwrap_or(("https", url));
     if schema == "file" {
-        return Ok(Some((
-            StatusCode::OK.to_string(),
-            HashMap::new(),
-            fs::read_to_string(url)?,
-        )));
+        return Ok(Response {
+            status: StatusCode::OK,
+            headers: None,
+            body: Some(fs::read(url)?),
+        });
     }
     if schema != "https" && schema != "http" {
-        return Ok(None);
+        return Err("Not Supoort Schema")?;
     }
     let (url, path) = url.split_once("/").unwrap_or((url, ""));
     let (host, url_port) = url.split_once(":").unwrap_or((url, ""));
@@ -54,7 +63,7 @@ fn online_access<S: Read + Write>(
     mut s: S,
     host: &str,
     path: &str,
-) -> Result<Option<(String, HashMap<String, String>, String)>, Error> {
+) -> Result<Response, Box<dyn Error>> {
     s.write_all(format!("GET /{} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\nUser-Agent: Browser-Demo/0.0.1\r\n\r\n", path, host).as_bytes())?;
     let mut reader = BufReader::new(s);
     let mut buf = String::new();
@@ -62,11 +71,11 @@ fn online_access<S: Read + Write>(
     let (version, status) = buf.split_once(" ").unwrap();
     let (status, explanation) = status.split_once(" ").unwrap();
     if status != "200" {
-        return Ok(Some((
-            status.to_string(),
-            HashMap::new(),
-            explanation.to_string(),
-        )));
+        return Ok(Response {
+            status: StatusCode::from_str(status)?,
+            headers: None,
+            body: Some(Vec::from(explanation)),
+        });
     }
     let mut headers: HashMap<String, String> = HashMap::new();
     for line in reader.by_ref().lines() {
@@ -77,14 +86,31 @@ fn online_access<S: Read + Write>(
         let (header, value) = line.split_once(":").unwrap();
         headers.insert(header.to_lowercase(), value.trim().to_string());
     }
-    let mut body = String::new();
-    reader.read_to_string(&mut body)?;
-    return Ok(Some((status.to_string(), headers, body)));
+    let mut body: Vec<u8> = Vec::new();
+    reader.read_to_end(&mut body)?;
+    return Ok(Response {
+        status: StatusCode::from_str(status)?,
+        headers: Some(headers),
+        body: Some(body),
+    });
 }
 
-fn show(body: &str) -> String {
+fn show(resp: Response) -> Result<String, Box<dyn Error>> {
+    if resp.status == StatusCode::OK {
+        if let Some(body) = resp.body {
+            show_only_body(body)
+        } else {
+            Ok(String::new())
+        }
+    } else {
+        show_without_tag(resp.status, resp.body.unwrap())
+    }
+}
+
+fn show_without_tag(status: StatusCode, body: Vec<u8>) -> Result<String, Box<dyn Error>> {
+    let body = String::from_utf8(body)?;
     let mut is_angle = 0;
-    let mut body_without_tag = String::new();
+    let mut body_without_tag = status.to_string();
     for c in body.chars() {
         if c == '<' {
             is_angle += 1;
@@ -94,23 +120,24 @@ fn show(body: &str) -> String {
             body_without_tag.push(c);
         }
     }
-    body_without_tag
+    Ok(body_without_tag)
 }
 
-fn parse_data(url: &str) -> Result<Option<(String, HashMap<String, String>, String)>, Error> {
+fn parse_data(url: &str) -> Result<Response, Box<dyn Error>> {
     let url = &url["data:".len()..];
     let (metadata, body) = url.split_once(",").unwrap();
     match metadata {
-        "text/html" => Ok(Some((
-            StatusCode::OK.to_string(),
-            HashMap::new(),
-            format!("<html><body>{}</body></html>", body),
-        ))),
-        _ => Ok(None),
+        "text/html" => Ok(Response {
+            status: StatusCode::OK,
+            headers: None,
+            body: Some(Vec::from(format!("<html><body>{}</body></html>", body))),
+        }),
+        _ => Err("Not Support Data Meta Type")?,
     }
 }
 
-fn show_only_body(body: &str) -> String {
+fn show_only_body(body: Vec<u8>) -> Result<String, Box<dyn Error>> {
+    let body = String::from_utf8(body)?;
     let mut is_angle = 0;
     let mut meet_body = false;
     let mut leave_body = false;
@@ -131,16 +158,13 @@ fn show_only_body(body: &str) -> String {
         } else if is_angle == 0 && meet_body && !leave_body {
             only_body.push(c);
         } else if is_angle == 1 {
-            tag.push(c);
+            tag.push(char::from(c));
         }
     }
-    only_body
+    Ok(only_body)
 }
-pub fn load(url: &str) -> String {
-    let (status, _, body) = request(url).unwrap().unwrap();
-    if status == StatusCode::OK.as_str() {
-        show_only_body(&body)
-    } else {
-        show(&body)
-    }
+
+pub fn load(url: &str) -> Result<String, Box<dyn Error>> {
+    let resp = request(url)?;
+    show(resp)
 }
